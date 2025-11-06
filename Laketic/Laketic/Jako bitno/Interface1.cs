39be1bbc68a06cc91eb8faad4240208a205182b1
Laketaa <luka.laketiic@gmail.com>
2025-11-06 22:19:33 +0100

    Dodat novi interfejs u posebnom folderu u projektu

---
+++ Laketic/Laketic/Jako bitno/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laketic.Jako_bitno
+{
+    internal interface Interface1
+    {
+    }
+}
